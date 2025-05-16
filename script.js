3833797b8448b386c0ca05bd0a1e2761a2f4e52f
SerAbinU1 <abinyt1234@gmail.com>
2025-05-16 17:24:46 +0000

    Done

---
+++ script.js
@@ -0,0 +1,30 @@
+let details = document.getElementById("details");
+let movie = document.getElementById("movie");
+
+movie.addEventListener("keypress", async (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        let movieDetails = await getDetails(movie.value);
+        if (movieDetails.Response === "False") {
+            details.textContent = movieDetails.Error;
+            return;
+        }
+        details.textContent = `
+        Title: ${movieDetails.Title}
+        Year: ${movieDetails.Year}
+        Genre: ${movieDetails.Genre}
+        Director: ${movieDetails.Director}
+        Writer: ${movieDetails.Writer}
+        Rated: ${movieDetails.Rated}`;
+    }
+});    
+
+async function getDetails(movieName) {
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=ea4913bc&t=${movieName}`);
+        const movieDetails = await response.json();
+        return movieDetails;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
